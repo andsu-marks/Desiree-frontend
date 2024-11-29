c0e2e9daf08d2a50efb31d15d05e8e5d0970123a
Anderson Marques Ramos <andsu.marks.r@gmail.com>
2024-11-29 09:02:53 -0300

    feature(login): temporary login added

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -5,16 +5,32 @@ import { useRedirect } from "../../hooks/useRedirect";
 import { Input, InputContainer, Label, SubmitButton } from "../../styles/EmployeeForm.styles";
 import { LoginForm } from "../../styles/Login.styles";
 import { LoginProps } from "../../types/LoginTypes";
-import { employeesFake } from "../../database";
 import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { listEmployeesService } from "../../services/listEmployeeService";
+import { EmployeesProps } from "../../types/EmployeeTypes";
 
 export function Login() {
+  const [employees, setEmployees] = useState<EmployeesProps[]>([]);
   const { goToPage } = useRedirect();
 
   const { formData, setFormData } = useFormData<LoginProps>({
     email: '',
     password: ''
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await listEmployeesService();
+        setEmployees(data);
+      } catch {
+        toast.error('Erro ao carregar perfil');
+      }
+    };
+    
+    fetchData();
+  }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
@@ -27,10 +43,10 @@ export function Login() {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const employee = employeesFake.find((emp) => (emp.email === formData.email) && (emp.password === formData.password));
+    const employee = employees.find((emp) => (emp.email === formData.email) && (emp.password === formData.password));
     
     if (!employee) {
-      toast.error('Funcionário não encontrado');
+      toast.error('Login e senha errados!');
       return;
     }
     
